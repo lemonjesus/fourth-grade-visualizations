6c30a61d910765139855ff1f3ba72eb92237eee2
Tucker Osman <osmiumusa@gmail.com>
2024-02-18 01:15:05 -0600

    finished implementing The Man

--- wmpplugin1/VizTheMan.cpp
+++ wmpplugin1/VizTheMan.cpp
@@ -14,12 +14,13 @@ class Man {
 public:
 	bool rest; // true if man can jump
 	float height; // man's current height
+	float prev_height; //man's height previous frame
 	float velocity; // man's current velocity
 
-	Man::Man():rest(true),height(0),velocity(0) {}
+	Man::Man():rest(true),height(0),prev_height(0),velocity(0) {}
 	Man::~Man() {}
 
-	void Man::render(ID2D1DeviceContext* m_d2dContext, ID2D1SolidColorBrush* m_pBrush, ID2D1SolidColorBrush* m_pBackgroundBrush, float x, float y, float target) {
+	void Man::render(ID2D1DeviceContext* m_d2dContext, ID2D1BitmapRenderTarget *bgEffectTarget, ID2D1SolidColorBrush* m_pBrush, ID2D1SolidColorBrush* m_pColorBrush, ID2D1SolidColorBrush* m_pBackgroundBrush, float x, float y, float target) {
 		// if man is at rest, set his v0 to meet his target, otherwise, continue with the previous jump
 		if(rest) {
 			velocity = -sqrt(2*GRAVITY*target);
@@ -38,6 +39,7 @@ public:
 		float army = height + y + 20 - velocity;
 		if(velocity > 10) army = height + y + 20 - 10;
 		if(velocity < -15) army = height + y + 20 + 15;
+		m_d2dContext->FillEllipse(head, m_pBackgroundBrush);
 		m_d2dContext->DrawEllipse(head, m_pBrush, 2);
 		m_d2dContext->DrawLine(D2D1::Point2F(x, height + y + 10), D2D1::Point2F(x, height + y + 30), m_pBrush, 2);
 		m_d2dContext->DrawLine(D2D1::Point2F(x, height + y + 20), D2D1::Point2F(x + 12, army), m_pBrush, 2);
@@ -50,6 +52,12 @@ public:
 		m_d2dContext->FillRectangle(D2D1::RectF(x - 5, height + y - 1, x + 5, height + y + 3), m_pBackgroundBrush);
 		m_d2dContext->DrawLine(D2D1::Point2F(x - 3, height + y - 1), D2D1::Point2F(x - 3, height + y - 4), m_pBrush, 1);
 		m_d2dContext->DrawLine(D2D1::Point2F(x + 3, height + y - 1), D2D1::Point2F(x + 3, height + y - 4), m_pBrush, 1);
+
+		bgEffectTarget->BeginDraw();
+		bgEffectTarget->DrawLine(D2D1::Point2F(x - 3, height + y - 10), D2D1::Point2F(x, prev_height + y - 10), m_pColorBrush, 2);
+		bgEffectTarget->EndDraw();
+
+		prev_height = height;
 	}
 };
 
@@ -95,15 +103,21 @@ void VizTheMan::render(
 	m_pBrush->SetColor(D2D1::ColorF(rgb.r/255.0f, rgb.g/255.0f, rgb.b/255.0f));
 
 	// background effects here
-	bgEffectTarget->BeginDraw();
-	bgEffectTarget->DrawBitmap(bitmap, rectf);
-	bgEffectTarget->EndDraw();
-
+	
 	m_d2dContext->BeginDraw();
 	m_d2dContext->Clear(D2D1::ColorF(1.0, 1.0, 1.0, 1.0));
-	// blur->SetInput(0, bgEffectBitmap);
-	// blur->SetValue(D2D1_GAUSSIANBLUR_PROP_STANDARD_DEVIATION, 1.0f);
-	// m_d2dContext->DrawImage(blur);
+	
+	blur->SetInput(0, bgEffectBitmap);
+	blur->SetValue(D2D1_GAUSSIANBLUR_PROP_STANDARD_DEVIATION, 0.4f);
+	m_d2dContext->DrawImage(blur);
+	m_d2dContext->Flush();
+
+	bgEffectTarget->BeginDraw();
+	displacedrect = D2D1::RectF(rectf.left + 3, rectf.top - 1, rectf.right + 3, rectf.bottom - 1);
+	bgEffectTarget->DrawBitmap(bitmap, &displacedrect);
+	bgEffectTarget->EndDraw();
+
+	// m_d2dContext->DrawBitmap(bgEffectBitmap);
 
 	int window = 1024/MEN_COUNT;
 	float interval = ((rectf.right - rectf.left) - 120) / MEN_COUNT;
@@ -118,7 +132,7 @@ void VizTheMan::render(
 
 		offset /= window * 2;
 
-		((Man**)men)[i]->render(m_d2dContext, m_pBlackBrush, m_pWhiteBrush, 60 + interval * i, 300, offset);
+		((Man**)men)[i]->render(m_d2dContext, bgEffectTarget, m_pBlackBrush, m_pBrush, m_pWhiteBrush, 60 + interval * i, 300, offset);
 	}
 
 	// draw the trampoline
--- wmpplugin1/VizTheMan.h
+++ wmpplugin1/VizTheMan.h
@@ -5,13 +5,13 @@ class VizTheMan : public Visualization {
 public:
 	ID2D1Effect *blur;
 	ID2D1SolidColorBrush *m_pBrush, *m_pBlackBrush, *m_pWhiteBrush;
+	D2D1_RECT_F displacedrect;
 	void* men;
 
 	VizTheMan(void);
 	~VizTheMan(void);
 
 	void init(ID2D1DeviceContext* m_d2dContext);
-	void drawMan(ID2D1DeviceContext* m_d2dContext, D2D_POINT_2F pos, float velocity);
 	void render(
 		unsigned long long frame,
 		D2D1_RECT_F rectf,
